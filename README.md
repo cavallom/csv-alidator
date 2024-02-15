a8ba8724f711ddd5962cd7c75efe945531d0ee0b
Massimo Cavallo <massimo.cavallo.62@gmail.com>
2024-02-15 17:13:53 +0100

    jsonValidation added

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ NPM package written in **JavaScript** to quickly validate a csv file based on **
   - [Usage](#usage)
     - [Validations](#validations)
       - [booleanValidation](#booleanvalidation)
+      - [jsonValidation](#jsonvalidation)
   - [Performance considerations](#performance-considerations)
   - [Comma-Separated Values (CSV)](#comma-separated-values-csv)
   - [License](#license)
@@ -32,7 +33,11 @@ Just to check the success of the installation, also returns basic package inform
 const csvalidator = require('csv-alidator');
 console.log(csvalidator.itWorks());
 
-#output : { json } > {"itWorks":"Yes, it works!","package":"csv-alidator","version":"1.0.1"} 
+#output : { json } > {
+#    "itWorks": "Yes, it works!",
+#    "package": "csv-alidator",
+#    "version": "1.0.3"
+#}
 ```
 
 ## Usage
@@ -54,6 +59,56 @@ const csvalidator = require('csv-alidator');
 console.log(csvalidator.booleanValidation('path-to-csv-file'));
 
 #output : { bool } > true | false
+```
+
+#### jsonValidation
+
+```bash
+/**
+ * Validates a CSV file and retunrs json with information about:
+ * execution result and time, CSV files and arrays containing the line numbers with error.
+ * Also very useful for quickly finding the wrong records among millions of lines.
+ *
+ * @param {string} csvFile - The path to the CSV file.
+ * @param {string} separator - The CSV separator character, not mandatory. Default value = ','.
+ * @param {boolean} ignoreEmptyRows - Non-mandatory parameter to handle the blank line at the end of the file.
+ */
+
+const csvalidator = require('csv-alidator');
+console.log(csvalidator.jsonValidation('path-to-csv-file'));
+
+#output { json } > successful validation: {
+#    "csvFile": "path-to-csv-file",
+#    "executiontime": "5496ms.",
+#    "rowsCount": 1000001,
+#    "columns": 9,
+#    "badRowsLines": [],
+#    "result": true,
+#    "message": ""
+#}
+
+#output { json } > failed validation: {
+#    "csvFile": "path-to-csv-file",
+#    "executiontime": "4817ms.",
+#    "rowsCount": 1000001,
+#    "columns": 9,
+#    "badRowsLines": [
+#        394579,
+#        941245
+#    ],
+#    "result": false,
+#    "message": ""
+#}
+
+#output { json } > failed no such file: {
+#    "csvFile": "path-to-csv-file",
+#    "executiontime": "1ms.",
+#    "rowsCount": null,
+#    "columns": null,
+#    "badRowsLines": null,
+#    "result": false,
+#    "message": "ENOENT: no such file or directory, open 'path-to-csv-file'"
+#}
 ```
 
 ## Performance considerations
--- index.js
+++ index.js
@@ -64,4 +64,92 @@ function booleanValidation(csvFile, separator = ',', ignoreEmptyRows = true) {
     }
 }
 
-module.exports = {itWorks, booleanValidation}
+/**
+ * Validates a CSV file and retunrs json with information about:
+ * execution result and time, CSV files and arrays containing the line numbers with error.
+ * Also very useful for quickly finding the wrong records among millions of lines.
+ *
+ * @param {string} csvFile - The path to the CSV file.
+ * @param {string} separator - The CSV separator character, not mandatory. Default value = ','.
+ * @param {boolean} ignoreEmptyRows - Non-mandatory parameter to handle the blank line at the end of the file.
+ */
+function jsonValidation(csvFile, separator = ',', ignoreEmptyRows = true) {
+
+    const start = Date.now();
+    let result = true;
+    let badRows = new Array();
+    let executiontime;
+
+    try
+    {
+        const pattern = /".*?"/g;
+    
+        const csvFileLines = csvFile =>
+        fs
+        .readFileSync(csvFile)
+        .toString('UTF8')
+        .split('\n')
+        .filter(function (el) {
+            if (ignoreEmptyRows) {
+                return el != '';
+            } else {
+                return el == el;
+            }
+        })
+        .map(function (row) {
+            while(patternFound = pattern.exec(row))
+            {
+                row = row.replace(patternFound, patternFound.toString().replace(',','@'));
+            }
+            return row.split(separator);
+        });
+    
+        let rowsInCsv = csvFileLines(csvFile).slice();
+        let firstLineLength = rowsInCsv[0].length;
+    
+        rowsInCsv.forEach(function callback(element, index) {
+            //console.log(`${index}: ${element}`);
+            if (element.length != firstLineLength) {
+                badRows.push(index + 1);
+                result = false;
+            }
+        });
+        
+        const end = Date.now();
+        executiontime = end - start;
+
+        if (badRows.length > 0) {
+            result = false;
+        } else {
+            result = true;
+        }
+    
+        //return result;
+        return JSON.stringify({ 
+            "csvFile": csvFile
+            , "executiontime" : `${(executiontime)}ms.`
+            , "rowsCount" : rowsInCsv.length
+            , "columns" : firstLineLength
+            , "badRowsLines": badRows
+            , "result" : result
+            , "message" : ""
+        });
+    } catch (error) {
+        const end = Date.now();
+        executiontime = end - start;
+        result = false;
+
+        //return result;
+        return JSON.stringify({ 
+            "csvFile": csvFile
+            , "executiontime" : `${(executiontime)}ms.`
+            , "rowsCount" : NaN
+            , "columns" : NaN
+            , "badRowsLines": NaN
+            , "result" : result
+            , "message" : error.message
+        });        
+    }
+}
+
+module.exports = {itWorks, booleanValidation, jsonValidation}
